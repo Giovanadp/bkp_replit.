5ae2d0fe7d140260f8cf687a212a1a84ee67c90e
Giovanadp <giovanadepauladomingues@gmail.com>
2024-10-17 16:32:35 -0300

    /INTRODUÇÃO-JS

---
+++ INTRODUCAO-JS/script.js
@@ -0,0 +1,15 @@
+const idade = 17
+const nome = "Carlos"
+const sobrenome = "Silva"
+const telefone = "41 99999-9999"
+const estaCasado = "true"
+// Crie uma variável para cada um dos 
+// dados listados : (nome, sobrenome, telefone, estaCasado= true ou false)
+// Imprima cada um dos valores com console.log(<variavel>)
+
+
+console.log(idade)
+console.log(nome)
+console.log(sobrenome)
+console.log(telefone)
+console.log(estaCasado)

bdce2b232c68719368745c00946190f8023af0f9
Giovanadp <giovanadepauladomingues@gmail.com>
2024-10-17 11:36:19 -0300

    /Array01

---
+++ Array01/script.js
@@ -0,0 +1,16 @@
+//VARIAVEIS UNITÁRIAS
+const nome = 'Jonas'
+const nome2 = 'Julia'
+const nome3 = 'Jessica'
+// ...
+
+
+
+
+//VARIAVEIS DE ARRAY
+const nomes = ['Jonas', 'Julia', 'Jessica']
+//IMPRIMINDO OS VALORES DO ARRAY
+console.log(nomes)
+console.log(nomes[2])
+nomes[10] = 'João'//CRIANDO DINAMICAMENTE
+console.log(nomes[10])
